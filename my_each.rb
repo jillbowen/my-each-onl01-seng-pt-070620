71adeed34dd70e886f15267df12096db0d73c5b0
Jill Short <jillian.b.short@gmail.com>
2020-07-15 23:40:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,7 @@
 def my_each(words) # put argument(s) here
- if block_given?
-    i = 0 
-      while i < array.length 
-        yield[i]
-        i = i + 1
-      end
-  else puts "No block was given!"
-  end
+   i = 0 
+    while i < array.length 
+      yield[i]
+      i = i + 1
+    end
 end
